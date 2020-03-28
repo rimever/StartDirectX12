8b6faa60796da3ec68a5634b18a85da3f7069b59
rimever <mot_siva_ordin@hotmail.com>
2020-03-29 00:02:53 +0900

    最初のプロジェクトを作成。

---
+++ StartDirectX12/main.cpp
@@ -0,0 +1,27 @@
+#include <Windows.h>
+#ifdef _DEBUG
+#include <iostream>
+#endif // _DEBUG
+
+using namespace std;
+
+
+void DebugOutputFormatString(const char* format, ...) {
+#ifdef _DEBUG
+	va_list valist;
+	va_start(valist, format);
+	printf(format,valist);
+	va_end(valist);
+#endif // _DEBUG
+
+}
+
+#ifdef _DEBUG
+int main() {
+# else
+int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int) {
+#endif
+	DebugOutputFormatString("Show window test.");
+	getchar();
+	return 0;
+}
